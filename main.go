cb61198a5d5a643178afa7519e06192e4a302090
JJFelix <felixomwenga441@gmail.com>
2025-01-27 19:04:20 +0300

    New commit - project structure, models, db connection setup

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"github.com/gin-gonic/gin"
+	"github.com/JJFelix/restaurant_management/database"
+	"github.com/JJFelix/restaurant_management/routes"
+	"github.com/JJFelix/restaurant_management/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+// mongodb
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main(){
+	port := os.Getenv("PORT")
+
+	if port ==""{
+		port = "8000"
+	}
+
+	// using gin router
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+
+
+}
